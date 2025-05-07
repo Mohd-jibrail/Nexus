81e02554beb92321b8584260d21dba718cfe70a7
Mohd Jibrail <mohdjibrail34@gmail.com>
2025-05-06 22:51:12 -0700

    fix: re-structre the code

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-from routes.auth import authentication_router
 
 app = FastAPI()
 
@@ -11,5 +10,3 @@ def get():
 @app.get("/hello")
 def get_hello():
     return JSONResponse(content={"message": "Hello Jibrail!"}, status_code=200)
-
-app.include_router(authentication_router, prefix="/api/v1/auth", tags=["auth"])
